30c3683c538d7cc126ca9220ca10734dd33ffca8
Ramiro Alfaro <ralfaro@nmetric.com>
2019-10-30 22:57:32 -0700

    minor cleanup of al messages

--- src/components/LeftSideMenu/sideMainMenu.js
+++ src/components/LeftSideMenu/sideMainMenu.js
@@ -5,13 +5,13 @@ const Sidemainenu = () => {
      return (
           <div className="side_menu_section">
                <ul className="menu_nav">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About Us</Link></li>
-                    <li><Link to="/services">Services</Link></li>
-                    <li><Link to="/portfolio">Portfolio</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/element">Element</Link></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About Us</a></li>
+                    <li><a href="/services">Services</a></li>
+                    <li><a href="/portfolio">Portfolio</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/element">Element</a></li>
                </ul>
           </div>
      )
